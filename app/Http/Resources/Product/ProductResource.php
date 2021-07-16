7437bfe3970764f8819ed3cdeec3bf45ca42a98a
pessin.ingadigital <carlos@ingadigital.com.br>
2021-07-16 08:37:29 -0300

    Product Collection Resource

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -14,7 +14,7 @@ class ProductResource extends JsonResource
             'price'         => $this->price,
             'stock'         => $this->stock == 0 ? 'Out of Stock' : $this->stock,
             'discount'      => $this->discount,
-            'total_price'   => round(( 1 - ($this->discount/100)) * $this->price, 2),
+            'totalPrice'    => round(( 1 - ($this->discount/100)) * $this->price, 2),
             'rating'        => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(), 2) : 'No rating yet',
             'href'          => [
                 'reviews' => route('reviews.index',$this->id)
