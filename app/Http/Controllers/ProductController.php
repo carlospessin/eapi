995f5bd357e2478610fe2fccf515fd05f8fcfbf3
pessin.ingadigital <carlos@ingadigital.com.br>
2021-07-19 09:46:29 -0300

    Create new Product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,13 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ProductRequest;
 use App\Http\Resources\Product\ProductCollection;
 use App\Http\Resources\Product\ProductResource;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProductController extends Controller
 {
+    // public function __construct()
+    // {
+    //     $this->middleware('auth')->except('index','show');
+    // }
+
+    public function __construct()
+    {
+      $this->middleware('auth:api', ['except' => ['index', 'show']]);
+    }
 
   public function index()
   {
@@ -20,9 +31,20 @@ class ProductController extends Controller
     //
   }
 
-  public function store(Request $request)
+  public function store(ProductRequest $request)
   {
-    //
+      $product = new Product;
+      $product->name = $request->name;
+      $product->detail = $request->description;
+      $product->stock = $request->stock;
+      $product->price = $request->price;
+      $product->discount = $request->discount;
+
+      $product->save();
+
+    return response([
+        'data' => new ProductResource($product)
+    ],Response::HTTP_CREATED);
   }
 
   public function show(Product $product)
